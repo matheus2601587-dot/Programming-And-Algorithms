29d3b201797c61986051ba8d2f441e3bc9b34942
matheus2601587-dot <matheus.2601587@aluno.impacta.edu.br>
2026-08-31 20:09:15 -0300

    Update if_else.py

--- Exercicio_de_Logica_de_Programação/if_else.py
+++ Exercicio_de_Logica_de_Programação/if_else.py
@@ -1,6 +1,43 @@
-print("--- Exemplo 1: Simples if ---")
+
+# Exemplo 1
 idade = 20
 print(f"Idade informada: {idade}")
 
 if idade >= 18:
     print("Acesso permitido. Você é maior de idade.")
+
+#Exemplo 2
+idade = 16
+print(f"Idade informada: {idade}")
+
+if idade >= 18:
+    print("Acesso permitido. Você é maior de idade.")
+else:
+    print("Acesso negado. Você é menor de idade.")
+
+#Exemplo 3
+nota = 75
+print(f"Nota do aluno: {nota}")
+
+if nota >= 90:
+    print("Conceito: A")
+elif nota >= 80:
+    print("Conceito: B")
+elif nota >= 70:
+    print("Conceito: C")
+elif nota >= 60:
+    print("Conceito: D")
+else:
+    print("Conceito: F (Reprovado)")
+
+#Exemplo 4
+temperatura = 28
+chovendo = False
+print(f"Condições atuais -> Temperatura: {temperatura}°C, Chovendo: {chovendo}")
+
+if temperatura > 25 and not chovendo:
+    print("Sugestão: Ótimo dia para ir à praia!")
+elif temperatura < 15 or chovendo:
+    print("Sugestão: Que tal um filme em casa?")
+else:
+    print("Sugestão: O tempo está agradável.")
